4a72d03dbbbfd9449284acc8b7018208e0fb0ac0
caoxiaosheng <836900367@qq.com>
2017-03-10 18:18:26 +0800

    添加项目文件。

---
+++ LiveChartsDemo/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace LiveChartsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Media.SolidColorBrush solidColorBrush1 = new System.Windows.Media.SolidColorBrush();
+            this.myChart = new LiveCharts.WinForms.CartesianChart();
+            this.ckb_BigPoint = new System.Windows.Forms.CheckBox();
+            this.ckb_SmallPoint = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // myChart
+            // 
+            this.myChart.Dock = System.Windows.Forms.DockStyle.Left;
+            this.myChart.Hoverable = true;
+            this.myChart.Location = new System.Drawing.Point(0, 0);
+            this.myChart.Name = "myChart";
+            solidColorBrush1.Color = System.Windows.Media.Color.FromArgb(((byte)(30)), ((byte)(30)), ((byte)(30)), ((byte)(30)));
+            this.myChart.ScrollBarFill = solidColorBrush1;
+            this.myChart.ScrollHorizontalFrom = 0D;
+            this.myChart.ScrollHorizontalTo = 0D;
+            this.myChart.ScrollMode = LiveCharts.ScrollMode.None;
+            this.myChart.ScrollVerticalFrom = 0D;
+            this.myChart.ScrollVerticalTo = 0D;
+            this.myChart.Size = new System.Drawing.Size(590, 425);
+            this.myChart.TabIndex = 0;
+            this.myChart.Text = "cartesianChart1";
+            // 
+            // ckb_BigPoint
+            // 
+            this.ckb_BigPoint.AutoSize = true;
+            this.ckb_BigPoint.Location = new System.Drawing.Point(676, 65);
+            this.ckb_BigPoint.Name = "ckb_BigPoint";
+            this.ckb_BigPoint.Size = new System.Drawing.Size(60, 16);
+            this.ckb_BigPoint.TabIndex = 1;
+            this.ckb_BigPoint.Text = "大数据";
+            this.ckb_BigPoint.UseVisualStyleBackColor = true;
+            this.ckb_BigPoint.CheckedChanged += new System.EventHandler(this.ckb_BigPoint_CheckedChanged);
+            // 
+            // ckb_SmallPoint
+            // 
+            this.ckb_SmallPoint.AutoSize = true;
+            this.ckb_SmallPoint.Location = new System.Drawing.Point(676, 104);
+            this.ckb_SmallPoint.Name = "ckb_SmallPoint";
+            this.ckb_SmallPoint.Size = new System.Drawing.Size(60, 16);
+            this.ckb_SmallPoint.TabIndex = 2;
+            this.ckb_SmallPoint.Text = "小数据";
+            this.ckb_SmallPoint.UseVisualStyleBackColor = true;
+            this.ckb_SmallPoint.CheckedChanged += new System.EventHandler(this.ckb_SmallPoint_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 425);
+            this.Controls.Add(this.ckb_SmallPoint);
+            this.Controls.Add(this.ckb_BigPoint);
+            this.Controls.Add(this.myChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart myChart;
+        private System.Windows.Forms.CheckBox ckb_BigPoint;
+        private System.Windows.Forms.CheckBox ckb_SmallPoint;
+    }
+}
+
---
+++ LiveChartsDemo/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using Brushes = System.Windows.Media.Brushes;
+
+namespace LiveChartsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private readonly Random random = new Random();
+
+        private LineSeries BigLineSeries { get; set; }
+        private LineSeries SmallLineSeries { get; set; }
+        private Axis BigAxis { get; set; }
+        private Axis SmallAxis { get; set; }
+        private List<DateTime> DateTimes { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myChart.Series.Configuration = Mappers.Xy<DateModel>()
+                .X(dayModel => (double)dayModel.DateTime.Ticks / TimeSpan.FromHours(1).Ticks)
+                .Y(dayModel => dayModel.Value);
+            DateTimes = CreateDateTime();
+            //myChart.Series = new SeriesCollection
+            //{
+            //    new LineSeries
+            //    {
+            //        Values = new ChartValues<double>() {3,5,double.NaN, 4, 7},
+            //        Stroke = Brushes.Red,
+            //        Fill = Brushes.Transparent,
+            //        StrokeThickness = 10
+            //    },
+            //    new ColumnSeries
+            //    {
+            //        Values = new ChartValues<decimal> {4, 3,  3, 1},
+            //        Stroke = Brushes.Blue,
+            //        Fill = Brushes.Chartreuse
+            //    }
+            //};
+        }
+
+        private List<DateModel> CreateBigPoint()
+        {
+            return CreatePoint(1000, 2000);
+        }
+
+        private List<DateModel> CreateSmallPoint()
+        {
+            return CreatePoint(1, 10);
+        }
+
+        private List<DateModel> CreatePoint(int minInt,int maxInt)
+        {
+            List<DateModel> dateModels = new List<DateModel>();
+            foreach (var dateTime in DateTimes)
+            {
+                dateModels.Add(new DateModel()
+                {
+                    DateTime = dateTime,
+                    Value = random.NextDouble() + random.Next(minInt, maxInt)
+                });
+            }
+            return dateModels;
+        }
+
+        private  List<DateTime> CreateDateTime()
+        {
+            List<DateTime> dateTimes=new List<DateTime>();
+            for (int i = 0; i < 500; i++)
+            {
+                dateTimes.Add(DateTime.Now.AddMonths(i).AddDays(random.Next(20)));
+            }
+            return dateTimes;
+        }
+
+        private void ckb_BigPoint_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckb_BigPoint.Checked)
+            {
+                var chartValues = new ChartValues<DateModel>();
+                chartValues.AddRange(CreateBigPoint());
+                BigLineSeries = new LineSeries { Values = chartValues, ScalesYAt = 0 };
+                myChart.Series.Add(BigLineSeries);
+                BigAxis = new Axis() { Foreground = Brushes.Blue, Title = ckb_BigPoint.Text };
+                myChart.AxisY.Add(BigAxis);
+            }
+            else
+            {
+                myChart.Series.Remove(BigLineSeries);
+                myChart.AxisY.Remove(BigAxis);
+            }
+            myChart.AxisX.Clear();
+            myChart.AxisX.Add(new Axis
+            {
+                LabelFormatter = value =>
+                {
+                    long tick = (long) (value * TimeSpan.FromHours(1).Ticks);
+                    if (tick > DateTime.MinValue.Ticks && tick < DateTime.MaxValue.Ticks)
+                    {
+                        return new System.DateTime(tick).ToString("yyyy-MM-dd hh");
+                    }
+                    else
+                    {
+                        return DateTime.Now.ToString("yyyy-MM-dd hh");
+                    }
+                }
+            });
+            //myChart.Series.Add(new LineSeries
+            //{
+            //    Values = new ChartValues<double> { 1, 5, 3, 5, 3 },
+            //    ScalesYAt = 0
+            //});
+            //myChart.Series.Add(new LineSeries
+            //{
+            //    Values = new ChartValues<double> { 20, 30, 70, 20, 10 },
+            //    ScalesYAt = 1
+            //});
+            //myChart.Series.Add(new LineSeries
+            //{
+            //    Values = new ChartValues<double> { 600, 300, 200, 600, 800 },
+            //    ScalesYAt = 2
+            //});
+
+            ////now we add the 3 axes
+
+            //myChart.AxisY.Add(new Axis
+            //{
+            //    Foreground = System.Windows.Media.Brushes.DodgerBlue,
+            //    Title = "Blue Axis"
+            //});
+            //myChart.AxisY.Add(new Axis
+            //{
+            //    Foreground = System.Windows.Media.Brushes.IndianRed,
+            //    Title = "Red Axis",
+            //    Position = AxisPosition.RightTop
+            //});
+            //myChart.AxisY.Add(new Axis
+            //{
+            //    Foreground = System.Windows.Media.Brushes.DarkOliveGreen,
+            //    Title = "Green Axis",
+            //    Position = AxisPosition.RightTop
+            //});
+
+        }
+
+        private void ckb_SmallPoint_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckb_BigPoint.Checked)
+            {
+                var chartValues = new ChartValues<DateModel>();
+                chartValues.AddRange(CreateSmallPoint());
+                SmallLineSeries = new LineSeries { Values = chartValues, ScalesYAt =1 };
+                myChart.Series.Add(SmallLineSeries);
+                SmallAxis= new Axis() { Foreground = Brushes.Black, Title = ckb_SmallPoint.Text };
+                myChart.AxisY.Add(SmallAxis);
+            }
+            else
+            {
+                myChart.Series.Remove(SmallLineSeries);
+                myChart.AxisY.Remove(SmallAxis);
+            }
+        }
+    }
+
+    public class DateModel
+    {
+        public DateTime DateTime { get; set; }
+        public double Value { get; set; }
+    }
+}
